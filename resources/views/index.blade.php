5c40761330ea4e4f89ab4e4bd8aa332a71a5ce09
pimenvibritania <pimenvibritania@gmail.com>
2021-11-23 22:58:06 +0700

    finish

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>test wedding</title>
+        <title>Yayang & Pimen Wedding</title>
         <link rel="icon" type="image/png" href="{{asset('assets/images/logo.png')}}" >
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
